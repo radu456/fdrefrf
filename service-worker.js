ceef6c97e4cfc070e7b3599c8c282ef69c37da62
radu456 <111914556+radu456@users.noreply.github.com>
2025-06-10 22:42:48 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+// service-worker.js
+const CACHE_NAME = 'biletul-perfect-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/assets/icon-192x192.png',
+  '/assets/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
